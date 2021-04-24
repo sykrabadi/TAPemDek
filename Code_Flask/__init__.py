5c277f5ed547b3f063cd235cc3364ca15247c1c0
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-04-24 09:27:35 +0700

    Additional method to display given input

--- Code_Flask/__init__.py
+++ Code_Flask/__init__.py
@@ -6,9 +6,11 @@ app.config['SECRET_KEY'] = '026c4ba5ad18bcc7b243b4d9'
 @app.route('/', methods=["GET","POST"])
 def home():
     if request.method == "POST":
-        sepal_length = request.form["sepal_length"]
-        sepal_width = request.form["sepal_width"]
-        petal_length = request.form["petal_length"]
-        petal_width = request.form["petal_width"]
-        n_neighbors = request.form["n_neighbors"]
-    return render_template('home.html')
+        sepal_length = request.form.get("sepal_length")
+        sepal_width = request.form.get("sepal_width")
+        petal_length = request.form.get("petal_length")
+        petal_width = request.form.get("petal_width")
+        n_neighbors = request.form.get("n_neighbors")
+        result = [sepal_length, sepal_width, petal_length, petal_width, n_neighbors]
+        
+    return render_template('home.html', result = result)
--- run.py
+++ run.py
@@ -2,3 +2,12 @@ from Code_Flask import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+"""
+Cara run app, di terminal, ketik:
+flask run
+
+Cara lain nge-run app, di terminal, ketik:
+$ENV:FLASK_APP = 'run.py'
+#ENV:FLASK_DEBUG = 1
+"""
